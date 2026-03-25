82a519bbdaa7aa7352885acbbfa0a9c0e0158aa4
Ademir Junior <maiante@outlook.com>
2026-03-25 01:52:23 -0300

    fix

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -5,7 +5,7 @@ import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-const connectionString = process.env.DATABASE_URL;
+const connectionString = process.env.DB_URL_OFFICIAL || process.env.DATABASE_URL;
 const pool = new Pool({ connectionString });
 const adapter = new PrismaPg(pool as any);
 
